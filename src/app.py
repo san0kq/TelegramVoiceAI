baa21463052675d3a120c111bed394f50417fcf5
san0kq <sanromanov94@gmail.com>
2024-06-02 19:44:27 +0300

    [upd] webhook deleted

--- src/app.py
+++ src/app.py
@@ -1,20 +1,14 @@
+import asyncio
 import logging
 import sys
-from contextlib import asynccontextmanager
-import uvicorn
-from typing import AsyncGenerator, Any
 
-from fastapi import FastAPI
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from config import settings
 from handlers import register_routes
 
 logger = logging.getLogger(__name__)
-
-WEBHOOK_PATH = f'/bot/{settings.tg_token}'
-WEBHOOK_URL = settings.webhook_url + WEBHOOK_PATH
 
 
 storage = MemoryStorage()
@@ -23,23 +17,13 @@ bot = Bot(token=settings.tg_token)
 dp = Dispatcher(storage=storage)
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI) -> AsyncGenerator[Any, Any]:
-    await bot.set_webhook(url=WEBHOOK_URL)
-
-    yield
-    await bot.delete_webhook()
-
-
-app = FastAPI(lifespan=lifespan)
-
 register_routes(dp)
 
 
-@app.post(WEBHOOK_PATH)
-async def bot_webhook(update: dict) -> None:
-    telegram_update = types.Update(**update)
-    await dp.feed_update(bot=bot, update=telegram_update)
+async def main() -> None:
+    bot = Bot(token=settings.tg_token)
+
+    await dp.start_polling(bot)
 
 
 if __name__ == '__main__':
@@ -49,4 +33,4 @@ if __name__ == '__main__':
         handlers=(logging.StreamHandler(sys.stdout), logging.FileHandler('file.log'))
     )
 
-    uvicorn.run(app, host=settings.app_host, port=settings.app_port)
+    asyncio.run(main())
